3207e711977193b2bf15ac4915e2c39996a6c2e3
Andrés Cecilia Luque <andres.luque@cellpointmobile.com>
2019-11-19 11:59:51 +0100

    Update swift package

--- Package.swift
+++ Package.swift
@@ -1,22 +1,25 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SpecGen",
+    products: [
+        .executable(name: "specgen", targets: ["SpecGen"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-package-manager.git", .revision("swift-DEVELOPMENT-SNAPSHOT-2019-03-04-a")),
-        .package(url: "https://github.com/Carthage/Carthage.git", .revision("0.33.0")),
+        .package(url: "https://github.com/Carthage/Carthage.git", .revision("0.34.0")),
     ],
     targets: [
         .target(
-            name: "specgen",
+            name: "SpecGen",
             dependencies: ["SPMUtility", "CarthageKit"]
         ),
         .testTarget(
             name: "SpecGenTests",
-            dependencies: ["specgen"]
+            dependencies: ["SpecGen"]
         ),
     ]
 )
